183fa4c0083cc99a6f9ea55a4a828eb25418f278
fran-bnd <fran-bnd@hotmail.com>
2023-02-03 00:38:14 +0000

    All AuthIntegrationTest test passed

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -61,7 +61,7 @@ public class AccountService {
      }
 
      public Account addAccount(Account account){
-        //- The registration will be successful if and only if the username is not blank, the password is at least 4 characters long, and an Account with that username does not already exist. If all these conditions are met, the response body should contain a JSON of the Account, including its account_id. The response status should be 200 OK, which is the default. The new account should be persisted to the database.
+        //The registration will be successful if and only if the username is not blank, the password is at least 4 characters long, and an Account with that username does not already exist. If all these conditions are met, the response body should contain a JSON of the Account, including its account_id. The response status should be 200 OK, which is the default. The new account should be persisted to the database.
         if (account.username == "") return null;
         if (account.password.length() < 4) return null;
 
@@ -69,8 +69,9 @@ public class AccountService {
      }
 
      public Account loginAccount(Account account){
-        //
-        return null;
+        //The login will be successful if and only if the username and password provided in the request body JSON match a real account existing on the database. 
+        
+        return accountDAO.loginIntoAccount(account);
      }
      
      
